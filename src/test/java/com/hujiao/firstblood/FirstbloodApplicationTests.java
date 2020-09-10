00a2e81b30a34aa846c76c3ee76a9676a840e7e1
hujiaofenr <1099321981@qq.com>
2020-09-11 02:04:20 +0800

    my firstblood

---
+++ src/test/java/com/hujiao/firstblood/FirstbloodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hujiao.firstblood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstbloodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
